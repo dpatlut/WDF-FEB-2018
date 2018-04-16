7c1f250eb93d058c96de80a6a54bbb5a78521d2b
David Patlut <david@theunpack.com>
2018-04-16 11:57:49 -0400

    pushing example code for post request

--- d34/Form Review/app.rb
+++ d34/Form Review/app.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
+require 'rubyjokes'
 
 get '/' do 
     erb :index
 end
 
+#Figure out how to pass a local that is nil becuase we get an error with the template
 post '/postroute' do 
-    puts params
+    joketype = params[:joketype]
+    joke = RubyJoke.new
+    @xyz = joke.get_joke({limitTo: [joketype]})
+    erb :index
+    # erb :index, :locals => {:joke => xyz}
 end
